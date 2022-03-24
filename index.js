50c6f754735cadea5cda7021589258d17d5988eb
satyamsharmaji <satyamsharma0414@gmail.com>
2022-03-24 10:27:40 +0530

    Integrate new features of newly otp input pattern

--- index.js
+++ index.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import { View, TextInput, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, TextInput, Text, Keyboard } from "react-native";
 
 const OtpPinInput = (props) => {
-    return (
-        <View>
-            {Array.from({ length: props.pin }).map((index) => {
-                return (
-                    <View>
-                        <Text>{props.value[index]}</Text>
-                    </View>
-                )
-            })}
-            <TextInput
-                value={props.value}
-                onChangeText={(text) => props.onChangeText(text)}
-                style={{
-                    width: '100%',
-                    height: 40,
-                    borderWidth: 1,
-                    borderColor: '#000'
-                }} />
-        </View>
-    )
-}
-export default OtpPinInput
+  const [focus, setFocus] = useState(0);
+  const [showBlinkingLine, setBlinkingLine] = useState(true);
+  const [inputFilled, setInputFilled] = useState(-1);
+
+  useEffect(() => {
+    const KeyboardShowEvent = Keyboard.addListener("keyboardDidShow", () => {
+      setFocus(props.value.length - 1);
+    });
+    const KeyboardHideEvent = Keyboard.addListener("keyboardDidHide", () => {
+      setFocus(-1);
+    });
+    setFocus(props.value.length - 1);
+    setInputFilled(props.value.length - 1);
+    return () => {
+      KeyboardShowEvent.remove();
+      KeyboardHideEvent.remove();
+    };
+  }, [props.value]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBlinkingLine((showBlinkingLine) => !showBlinkingLine);
+    }, 800);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: 40,
+        position: "relative",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          width: "100%",
+          justifyContent: "space-between",
+          position: "absolute",
+          zIndex: 9,
+        }}
+      >
+        {Array.from({ length: props.pin }).map((item, index) => {
+          return (
+            <View
+              key={index + "_input"}
+              style={{
+                width: 40,
+                height: 40,
+                borderWidth: focus === index ? 2 : 1,
+                borderColor: "#000",
+                borderRadius: 10,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: inputFilled >= index ? "#ccc" : "transparent",
+                // borderBottomColor: focus === index ? "red" : '#000',
+                // borderBottomWidth: focus === index ? 2 : 1
+              }}
+            >
+              <Text>{props.value[index]}</Text>
+            </View>
+          );
+        })}
+      </View>
+      <TextInput
+        autoFocus
+        caretHidden={true}
+        value={props.value}
+        onChangeText={(text) => {
+          props.onChange(text);
+          if (text.length == props.pin) {
+            props.onFilled();
+          }
+        }}
+        style={{
+          width: "100%",
+          height: 40,
+          opacity: 0,
+        }}
+        maxLength={props.pin}
+        keyboardType={"number-pad"}
+      />
+    </View>
+  );
+};
+export default OtpPinInput;
